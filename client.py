c3146ad414fbe82957272bc779e032f9a4c8030c
nouarnoorelhouda <nouarnoorelhouda@gmail.com>
2020-04-29 17:17:21 +0200

    Update client.py

--- client.py
+++ client.py
@@ -3,14 +3,16 @@ import os
 import subprocess
 
 s = socket.socket()
-host = '104.236.209.167'
+host = '127.0.0.1'
 port = 9999
 
 s.connect((host, port))
 
-filename = "test.txt"
+filename = 'test.txt'
 file = open(filename, 'wb')
 file_data = s.recv(1024)
 file.write(file_data)
 file.close()
 print("File has been received successfully")
+s.close()
+
